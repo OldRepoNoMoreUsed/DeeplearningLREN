497e6cb45e13b1f2b66914723460889834d1b64e
SommerNicolas <nicolas.sommer@he-arc.ch>
2017-07-19 16:19:21 +0200

    ajout d'un fichier bash pour trouver et extraire les données IRM

---
+++ ExtractNifti.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+find neuro_morphometric_atlas -type f -iname "*.nii" | xargs cp -t IRM_Experience
--- src/main/java/wrapper/local/LocalWrapperDl4j.java
+++ src/main/java/wrapper/local/LocalWrapperDl4j.java
@@ -1,12 +1,8 @@
 package wrapper.local;
 
-import org.deeplearning4j.api.storage.StatsStorage;
 import org.deeplearning4j.datasets.iterator.INDArrayDataSetIterator;
 import org.deeplearning4j.eval.Evaluation;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
-import org.deeplearning4j.ui.api.UIServer;
-import org.deeplearning4j.ui.stats.StatsListener;
-import org.deeplearning4j.ui.storage.InMemoryStatsStorage;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
 import wrapper.WrapperDl4j;
@@ -15,14 +11,9 @@ import wrapper.WrapperDl4j;
  * Created by nicolas on 13.07.17.
  */
 public class LocalWrapperDl4j extends WrapperDl4j{
-    private StatsStorage statsStorage;
-    private UIServer uiServer;
 
     public LocalWrapperDl4j(int seed){
         super(seed);
-        this.statsStorage = new InMemoryStatsStorage();
-        this.uiServer = UIServer.getInstance();
-        uiServer.attach(statsStorage);
     }
 
     public void init(){
